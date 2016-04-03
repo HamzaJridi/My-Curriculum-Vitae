f2ca1af000989f7963ccc55eef4978ad0ec9eb3a
Hamza Jridi <jrid_hamza@yahoo.fr>
2016-04-03 18:04:08 +0100

    home page done

--- js/controllers.js
+++ js/controllers.js
@@ -1,15 +1,46 @@
 // créer le module "myCV"
 var myCV = angular.module('myCV', ['ngRoute']);
+
+//configurer les routes
+myCV.config(function($routeProvider) {
+    $routeProvider
+
+        // route pour l'acceuil
+        .when('/', {
+            templateUrl : 'routes/home.html',
+            controller  : 'headCtrl'
+        })
+
+        // route pour competences.html
+        .when('/competences', {
+            templateUrl : 'routes/competences.html',
+            controller  : 'comptCtrl'
+        })
+
+
+        .otherwise({
+            redirectTo:'/'
+        });
+});
+
 //créer le controleur et injecter le service $scope et les données
-myCV.controller('headCtrl', function($scope) {
+myCV.controller('headCtrl', function ($scope) {
     $scope.data =
     {
         "adresse" : "La Soukra, Ariana Tunisie",
         "e-mail" : "hamzaJridi.h@gmail.com",
         "tel" : "(+216) 22 428 402",
         "linkedIN" : "https://tn.linkedin.com/in/jridihamza",
-        "Expérience professionnelle":[
+        "resume": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt  culpa qui officia deserunt mollit anim  est laborum."
+    }
+});
 
-        ] ,
+myCV.controller('comptCtrl', function($scope) {
+    $scope.compt =
+    {
+        "date" : "Septembre 2015 - Présent",
+        "role" : "Membre dans le cadre collaboratif « Teampany »",
+        "tache" : "Développement des plateformes Web ",
+        "tech" : "HTML5, CSS3, JavaScript, jQuery, Node.js, Socket.io, Redis, HandsOnTable"
     }
 });
